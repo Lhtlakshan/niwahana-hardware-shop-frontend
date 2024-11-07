10f54ae8b08c2d24bbd53c95af8989540a35dfd5
Thilina <thilinalakshanhewage1999@gmail.com>
2024-11-07 09:56:44 +0530

    apply a coupon UI

--- src/app/customer/components/cart/cart.component.ts
+++ src/app/customer/components/cart/cart.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CustomerService } from '../../service/customer.service';
 import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [NgIf,NgFor],
+  imports: [NgIf,NgFor,ReactiveFormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
@@ -15,12 +15,26 @@ export class CartComponent {
   cart:any[] = [];
   order:any;
 
+  couponForm: FormGroup
+
   constructor(private customerService:CustomerService
     ,private fb:FormBuilder
   ){}
 
   ngOnInit(){
+    this.couponForm = this.fb.group({
+      code:[null,[Validators.required]]
+    })
     this.getCart();
+  }
+
+  applyCoupon(){
+    this.customerService.applyCoupon(this.couponForm.get(['code']).value).subscribe(res=>{
+      alert("Coupon applied successfully");
+      this.getCart();
+    },err=>{
+      alert("Cannot apply coupon");
+    })
   }
 
   getCart(){
@@ -32,22 +46,8 @@ export class CartComponent {
       res.cartItems.forEach(element => {
         this.cart.push(element);
       });
-      console.log(this.cart);
-      
+      console.log(this.cart); 
     })
     
   }
-
-  // getAllProducts(){
-  //   this.products = [];
-  //   this.customerService.getAllProducts().subscribe(res=>{
-  //     Object.values(res).forEach(element=>{
-  //       this.products.push(element)
-  //     })
-  //     console.log(this.products);
-      
-  //   })
-  // }
-
-
 }
